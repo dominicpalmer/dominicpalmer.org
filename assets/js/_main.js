61536cc54b540e2ef1d8ad61806ac0f056791608
Dominic Palmer <dominic.palmer@outlook.com>
2021-06-05 21:39:09 +0100

    Updated jQuery dependency with new nav fix

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,18 +3,6 @@
    ========================================================================== */
 
 $(document).ready(function() {
-  // Sticky footer
-  var bumpIt = function() {
-    $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
-  };
-
-  bumpIt();
-  $(window).resize(
-    jQuery.throttle(250, function() {
-      bumpIt();
-    })
-  );
-
   // FitVids init
   $("#main").fitVids();
 
@@ -74,7 +62,7 @@ $(document).ready(function() {
   });
 
   // Gumshoe scroll spy init
-  if ($("nav.toc").length > 0) {
+  if($("nav.toc").length > 0) {
     var spy = new Gumshoe("nav.toc a", {
       // Active classes
       navClass: "active", // applied to the nav list item
